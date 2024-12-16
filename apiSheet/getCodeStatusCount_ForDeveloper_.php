87b60db462fe0ce1ffc844e8e20d9e6e6755775c
Amarfio <joshuaamarfio1@outlook.com>
2024-12-16 10:06:05 +0000

    updated for good with hashtag search

--- apiSheet/getCodeStatusCount_ForDeveloper_.php
+++ apiSheet/getCodeStatusCount_ForDeveloper_.php
@@ -40,15 +40,15 @@ function get_status_count($status_id, $user_id, $conn){
     // $query = "SELECT COUNT(t.status) total_task_status FROM tasks t WHERE t.is_approved = 1 AND t.status = '$status_id'";
     
     // $query = "";
-    $query = "SELECT COUNT(p.status) total_project_count FROM projects p WHERE  p.status  = '$status_id' AND (p.owner = '$user_id' AND p.is_archive=0 )";
+    $query = "SELECT COUNT(p.status) total_project_count FROM projects p WHERE  p.status  = '$status_id' AND (p.owner = '$user_id' OR p.s_owner='$user_id' AND p.is_archive=0 )";
 
     if($status_id == 116 ){
         // $query = "SELECT COUNT(project_id) total_overdue FROM projects WHERE NOW()>end_date AND dept_id = '$department_id'";
-        $query = "SELECT COUNT(p.status) total_project_count FROM projects p WHERE (CURRENT_DATE > end_date AND p.is_approved=1) AND (SELECT AVG(t.completion)<100 from tasks t WHERE t.project_id = p.project_id) AND (p.owner = '$user_id' AND p.is_archive=0)";
+        $query = "SELECT COUNT(p.status) total_project_count FROM projects p WHERE (CURRENT_DATE > end_date AND p.is_approved=1) AND (SELECT AVG(t.completion)<100 from tasks t WHERE t.project_id = p.project_id) AND (p.owner = '$user_id' OR p.s_owner='$user_id' AND p.is_archive=0)";
     }
     elseif($status_id == 85 ){
         // $query = "SELECT COUNT(project_id) total_overdue FROM projects WHERE NOW()>end_date AND dept_id = '$department_id'";
-        $query = "SELECT COUNT(p.status) total_project_count FROM projects p WHERE ( p.is_approved=1) AND (p.owner = '$user_id' AND p.is_archive=0)";
+        $query = "SELECT COUNT(p.status) total_project_count FROM projects p WHERE ( p.is_approved=1) AND (p.owner = '$user_id' OR p.s_owner='$user_id' AND p.is_archive=0)";
     }
     // elseif($status_id == 88){
     //     $query = "SELECT COUNT(p.status) total_project_count FROM projects p WHERE (p.is_approved=1) AND (SELECT AVG(t.completion)=100 from tasks t WHERE t.project_id = p.project_id) AND p.dept_id = '$department_id'";

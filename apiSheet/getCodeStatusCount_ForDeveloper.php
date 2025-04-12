e29e32dd1a6bb2f6137cdfe74af927e95af8a401
Joshua Amarfio <joshuaamarfio1@gmail.com>
2025-04-12 16:26:30 +0000

    updated with the correct stats for projects number

--- apiSheet/getCodeStatusCount_ForDeveloper.php
+++ apiSheet/getCodeStatusCount_ForDeveloper.php
@@ -1,4 +1,3 @@
-
 <?php
 
 header('Access-Control-Allow-Origin: *');
@@ -18,7 +17,7 @@ function get_total_project_count( $conn, $department_id, $user_id){
     // $query = "SELECT attach FROM comments c WHERE c.task_id = '$task_id' AND c.attach != ''";
     // $query = "SELECT COUNT(t.status) total_task_status FROM tasks t WHERE t.is_approved = 1 AND t.status = '$status_id'";
     // $query = "SELECT COUNT(p.project_id) total_project_count FROM projects p WHERE  p.dept_id = '$department_id' AND p.owner = '$user_id' ";
-    $query = "SELECT COUNT(p.project_id) total_project_count FROM projects p WHERE (p.owner = '$user_id' OR p.s_owner = '$user_id') AND p.is_archive = 0 ";
+    $query = "SELECT COUNT(p.project_id) total_project_count FROM projects p WHERE (p.owner = '$user_id' OR p.s_owner = '$user_id' OR p.posted_by = '$user_id') AND p.is_archive = 0 ";
     $result = mysqli_query($conn, $query); 
     // $num = mysqli_num_rows($result);
     $count_total_status = array();
@@ -123,4 +122,3 @@ if (isset($_GET['init'])  ) {
         echo 'empty';
     }
 }
-
